70f238134eb24af29bc7c88b5d9d9a2fe0b1ff38
bmanisha12 <b.manisha59@gmail.com>
2016-03-21 23:54:11 +0530

    Added files via upload

---
+++ patt.java
@@ -0,0 +1,31 @@
+
+public class patt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i , j , k;
+		
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=i;j++)
+			{		System.out.print(j);
+		
+			}
+			System.out.println();
+		}
+
+		for(i=4;i>=1;i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+		System.out.println();
+		}
+	
+	}
+}
